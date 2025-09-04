b604f402704999410a56a12541ba38eac91d78a7
suman1040 <uemj2003@gmail.com>
2025-09-04 13:54:24 +0530

    Create SignIn & SignUp Activity

--- app/src/main/java/com/example/sahaayak/auth/SignInActivity.kt
+++ app/src/main/java/com/example/sahaayak/auth/SignInActivity.kt
@@ -1,3 +1,9 @@
+/*
+ * Name: Suman Kumar Dey
+ * Info: Built a women safety app “SafeCircle” in Kotlin + XML integrating sensor data, mic audio, and an on-device ML model to detect real-time danger. Auto-triggers SMS, location sharing, and alarm on risk detection. Showcased advanced Android and edge ML integration.
+ * Connect with me: www.linkedin.com/in/suman1040
+ */
+
 package com.example.sahaayak.auth
 
 import android.content.Intent
